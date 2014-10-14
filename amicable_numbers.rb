967a45ae8a9c2f60afeb2bbf555db798cd6ae2b4
Troy Burroughs <burroughs.troy@gmail.com>
2014-10-13 23:36:27 -0500

    Update amicable_numbers.rb

--- amicable_numbers.rb
+++ amicable_numbers.rb
@@ -1,22 +1,26 @@
 def sum_factors(n)
-  sum = 0
+  sum1 = 0
+  # Find the sum (sum1) of the factors of n
   for factor in 1..(n / 2)
     if n % factor == 0
-      sum += factor
+      sum1 += factor
     end
   end
-  sum1 = 0
-  for factor in 1..(sum / 2)
-    if sum % factor == 0
-    sum1 += factor
+  sum2 = 0
+  # Find the sum (sum2) of all the factors of sum1
+  for factor in 1..(sum1 / 2)
+    if sum1 % factor == 0
+    sum2 += factor
     end
   end
-  if sum1 == n && n < sum
-    print n," ", sum, "\n"
+  # If n is equal to sum2 print the pair of amicable numbers
+  if sum2 == n && n < sum1
+    print n," ", sum1, "\n"
   end
 end
-print "Enter upper bound for perfect number search:"
+
+print "Enter a number a number greater than 1000 to find an amicable pair:"
 upper_bound = gets.chomp.to_i
-for n in 2..upper_bound
-  k = sum_factors(n)
+for n in 1..upper_bound
+  sum_factors(n)
 end
